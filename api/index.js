83bce32da12fdef791819f5bf70ea6c42b8993ee
aravelha <rahelveralda@gmail.com>
2025-06-06 22:54:54 +0700

    index comment

--- api/index.js
+++ api/index.js
@@ -14,6 +14,19 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(Router);
 
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>📚 Litera API is running!</h1>
+    <p>Welcome to Litera's backend!</p>
+    <p>Try running: <code>https://backend-litera.vercel.app/books/search?q=(judul+judul)</code></p>
+    <h3>Example:</h3>
+    <p><a href="https://backend-litera.vercel.app/books/search?q=the+stranger" target="_blank">https://backend-litera.vercel.app/books/search?q=the+stranger</a></p>
+    <p><a href="https://backend-litera.vercel.app/books/search?q=the+stranger" target="_blank">https://backend-litera.vercel.app/books/search?q=laut+bercerita</a></p>
+  `);
+});
+
+
+
 app.listen(3000, () => {
   console.log(`server berjalan di http://localhost:3000`);
 });
